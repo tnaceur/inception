7f0fbf4fab15dadadc3f84579ac057b39c476faa
tnaceur <tnaceur@tnaceur.42.fr>
2023-08-18 17:28:54 +0100

    Inception

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mariadb/mariadb.conf.d/50-server.cnf
+# sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mariadb start
 
-sleep 5
-
-mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -u root -h localhost -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -u root -h localhost -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -u root -h localhost -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mariadb -u root -h localhost -e "FLUSH PRIVILEGES;"
 
 service mariadb stop
 
-mariadbd
+mariadbd --bind-address=0.0.0.0
